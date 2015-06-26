9efe8208964a9005cfaf6f55fd663cd5bb63d0de
Frank Tan <Francis.tine@gmail.com>
2015-06-26 23:23:02 +0800

    Added weather detail activity and fragment

--- app/src/main/java/com/franktan/androidsunshine/app/ForecastFragment.java
+++ app/src/main/java/com/franktan/androidsunshine/app/ForecastFragment.java
@@ -1,6 +1,7 @@
 package com.franktan.androidsunshine.app;
 
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -10,6 +11,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -22,7 +24,7 @@ import java.util.Arrays;
  */
 public class ForecastFragment extends Fragment {
 
-    private ArrayAdapter<String> adapter;
+    private ArrayAdapter<String> forecastAdapter;
     private ListView forecastListView;
     private static final String LOG_TAG = "androidsunshine";
 
@@ -42,6 +44,18 @@ public class ForecastFragment extends Fragment {
         new FetchWeatherAsyncTask(this).execute("94043");
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         forecastListView = (ListView) view.findViewById(R.id.listview_forecast);
+        forecastListView.setOnItemClickListener(
+            new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    String text = forecastAdapter.getItem(position);
+
+                    Intent goToDetailPage = new Intent(getActivity(), WeatherDetailActivity.class);
+                    goToDetailPage.putExtra("detailWeatherText",text);
+                    startActivity(goToDetailPage);
+                }
+            }
+        );
 
         return view;
     }
@@ -63,14 +77,14 @@ public class ForecastFragment extends Fragment {
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public void setServerData(String[] result){
         ArrayList<String> resultArrayList = new ArrayList<String>(Arrays.asList(result));
-        if(adapter == null) {
-            adapter = new ArrayAdapter<String>(getActivity(),
+        if(forecastAdapter == null) {
+            forecastAdapter = new ArrayAdapter<String>(getActivity(),
                     R.layout.list_item_forcast, R.id.list_item_forecast_textview, resultArrayList);
 
-            forecastListView.setAdapter(adapter);
+            forecastListView.setAdapter(forecastAdapter);
         } else {
-            adapter.clear();
-            adapter.addAll(resultArrayList);
+            forecastAdapter.clear();
+            forecastAdapter.addAll(resultArrayList);
         }
     }
 }
---
+++ app/src/main/java/com/franktan/androidsunshine/app/WeatherDetailFragment.java
@@ -0,0 +1,35 @@
+package com.franktan.androidsunshine.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class WeatherDetailFragment extends Fragment {
+
+    public WeatherDetailFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_weather_detail, container, false);
+        
+        Intent intent = getActivity().getIntent();
+        TextView detailWeatherText = (TextView) view.findViewById(R.id.detailWeatherText);
+        detailWeatherText.setText(intent.getExtras().getString("detailWeatherText"));
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+}

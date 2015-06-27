611783ab6928eb87a1a127f811a6789206e03e62
Frank Tan <Francis.tine@gmail.com>
2015-06-27 17:00:52 +0800

    Removed debugging info

--- app/src/main/java/com/franktan/androidsunshine/app/WeatherDetailFragment.java
+++ app/src/main/java/com/franktan/androidsunshine/app/WeatherDetailFragment.java
@@ -6,14 +6,13 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.MenuItemCompat;
-import android.util.Log;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v7.widget.ShareActionProvider;
 import android.widget.TextView;
 
 
@@ -48,26 +47,20 @@ public class WeatherDetailFragment extends Fragment {
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        Log.i(MainActivity.LOG_TAG,"before inflate");
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.menu_weather_detail, menu);
-        Log.i(MainActivity.LOG_TAG,"after inflate");
         MenuItem item = menu.findItem(R.id.action_share);
         mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
-        Log.i(MainActivity.LOG_TAG,"after getting ShareActionProvider");
 
         setShareIntent();
     }
 
     private void setShareIntent () {
-        Log.i(MainActivity.LOG_TAG,"in setShareIntent");
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
         shareIntent.putExtra(Intent.EXTRA_TEXT, detailWeatherText);
         shareIntent.setType("text/plain");
-        Log.i(MainActivity.LOG_TAG, "checking mShareActionProvider ");
         if (mShareActionProvider != null) {
-            Log.i(MainActivity.LOG_TAG,"to setShareIntent");
             mShareActionProvider.setShareIntent(shareIntent);
         }
     }

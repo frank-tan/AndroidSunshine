f7bec1d3030bf614ce19d877c302d220e6396e68
Frank Tan <Francis.tine@gmail.com>
2015-06-30 19:03:14 +0800

    Implemented cursor loader to fetch data from content provider

--- app/src/main/java/com/franktan/androidsunshine/app/ForecastFragment.java
+++ app/src/main/java/com/franktan/androidsunshine/app/ForecastFragment.java
@@ -2,10 +2,14 @@ package com.franktan.androidsunshine.app;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,20 +17,46 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.ArrayList;
+import com.franktan.androidsunshine.app.data.WeatherContract;
 
-
-/**
- * A placeholder fragment containing a simple view.
- */
-public class ForecastFragment extends Fragment {
+public class ForecastFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
     private SharedPreferences sharedPreferences;
-    private ArrayAdapter forecastAdapter;
+    private ForecastAdapter forecastAdapter;
     private ListView forecastListView;
     private static final String LOG_TAG = "androidsunshine";
+    private static final int FORECAST_LOADER_ID = 0;
+
+    private static final String[] FORECAST_COLUMNS = {
+            // In this case the id needs to be fully qualified with a table name, since
+            // the content provider joins the location & weather tables in the background
+            // (both have an _id column)
+            // On the one hand, that's annoying.  On the other, you can search the weather table
+            // using the location set by the user, which is only in the Location table.
+            // So the convenience is worth it.
+            WeatherContract.WeatherEntry.TABLE_NAME + "." + WeatherContract.WeatherEntry._ID,
+            WeatherContract.WeatherEntry.COLUMN_DATE,
+            WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+            WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING,
+            WeatherContract.WeatherEntry.COLUMN_WEATHER_ID,
+            WeatherContract.LocationEntry.COLUMN_COORD_LAT,
+            WeatherContract.LocationEntry.COLUMN_COORD_LONG
+    };
+
+    // These indices are tied to FORECAST_COLUMNS.  If FORECAST_COLUMNS changes, these
+    // must change.
+    static final int COL_WEATHER_ID = 0;
+    static final int COL_WEATHER_DATE = 1;
+    static final int COL_WEATHER_DESC = 2;
+    static final int COL_WEATHER_MAX_TEMP = 3;
+    static final int COL_WEATHER_MIN_TEMP = 4;
+    static final int COL_LOCATION_SETTING = 5;
+    static final int COL_WEATHER_CONDITION_ID = 6;
+    static final int COL_COORD_LAT = 7;
+    static final int COL_COORD_LONG = 8;
 
     public ForecastFragment() {
     }
@@ -36,34 +66,76 @@ public class ForecastFragment extends Fragment {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getBaseContext());
+
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        getLoaderManager().initLoader(FORECAST_LOADER_ID, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String locationSetting = Utility.getPreferredLocation(getActivity());
+        String sortOrder = WeatherContract.WeatherEntry.COLUMN_DATE + " ASC";
+        Uri weatherForLocationUri = WeatherContract.WeatherEntry.buildWeatherLocationWithStartDate(
+                locationSetting,
+                System.currentTimeMillis()
+        );
+        return new CursorLoader(
+                getActivity(),
+                weatherForLocationUri,
+                FORECAST_COLUMNS,
+                null,
+                null,
+                sortOrder
+        );
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        forecastAdapter.swapCursor(cursor);
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+        forecastAdapter.swapCursor(null);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        forecastAdapter = new ArrayAdapter<String>(
-                getActivity(),
-                R.layout.list_item_forcast,
-                R.id.list_item_forecast_textview,
-                new ArrayList<String>());
+
+        forecastAdapter = new ForecastAdapter(getActivity(), null, 0);
+
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         forecastListView = (ListView) view.findViewById(R.id.listview_forecast);
         forecastListView.setAdapter(forecastAdapter);
-        forecastListView.setOnItemClickListener(
-                new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        String text = (String)forecastAdapter.getItem(position);
 
-                        Intent goToDetailPage = new Intent(getActivity(), WeatherDetailActivity.class);
-                        goToDetailPage.putExtra("detailWeatherText", text);
-                        startActivity(goToDetailPage);
-                    }
+        forecastListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView adapterView, View view, int position, long l) {
+                // CursorAdapter returns a cursor at the correct position for getItem(), or null
+                // if it cannot seek to that position.
+                Cursor cursor = (Cursor) adapterView.getItemAtPosition(position);
+                if (cursor != null) {
+                    String locationSetting = Utility.getPreferredLocation(getActivity());
+                    Intent intent = new Intent(getActivity(), WeatherDetailActivity.class)
+                            .setData(WeatherContract.WeatherEntry.buildWeatherLocationWithDate(
+                                    locationSetting, cursor.getLong(COL_WEATHER_DATE)
+                            ));
+                    startActivity(intent);
                 }
-        );
+            }
+        });
 
         return view;
     }
+
 
     @Override
     public void onStart() {
@@ -74,7 +146,6 @@ public class ForecastFragment extends Fragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.main, menu);
-        //inflater.inflate(R.menu.forecastfragment, menu);
     }
 
     @Override
@@ -105,24 +176,11 @@ public class ForecastFragment extends Fragment {
         }
         return super.onOptionsItemSelected(item);
     }
-//    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-//    public void onPostExecute(String[] weatherDataArray){
-//        ArrayList<String> resultArrayList = new ArrayList<String>(Arrays.asList(weatherDataArray));
-//        if(forecastAdapter == null) {
-//            forecastAdapter = new ArrayAdapter<String>(getActivity(),
-//                    R.layout.list_item_forcast, R.id.list_item_forecast_textview, resultArrayList);
-//
-//            forecastListView.setAdapter(forecastAdapter);
-//        } else {
-//            forecastAdapter.clear();
-//            forecastAdapter.addAll(resultArrayList);
-//        }
-//    }
+
     private void updateWeather () {
-        String postCode = sharedPreferences.getString(
-                getString(R.string.pref_location_key),
-                getString(R.string.pref_location_default));
-        new FetchWeatherTask(getActivity(),forecastAdapter).execute(postCode);
+        FetchWeatherTask weatherTask = new FetchWeatherTask(getActivity());
+        String location = Utility.getPreferredLocation(getActivity());
+        weatherTask.execute(location);
     }
 
 }
--- app/src/main/java/com/franktan/androidsunshine/app/WeatherDetailFragment.java
+++ app/src/main/java/com/franktan/androidsunshine/app/WeatherDetailFragment.java
@@ -2,9 +2,13 @@ package com.franktan.androidsunshine.app;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
@@ -15,11 +19,36 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.franktan.androidsunshine.app.data.WeatherContract;
 
-public class WeatherDetailFragment extends Fragment {
+
+public class WeatherDetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
     private SharedPreferences sharedPreferences;
     private ShareActionProvider mShareActionProvider;
-    String detailWeatherText;
+    String mDetailWeatherText;
+    private static final int FORECAST_LOADER_ID = 0;
+
+    private static final String[] FORECAST_COLUMNS = {
+            // In this case the id needs to be fully qualified with a table name, since
+            // the content provider joins the location & weather tables in the background
+            // (both have an _id column)
+            // On the one hand, that's annoying.  On the other, you can search the weather table
+            // using the location set by the user, which is only in the Location table.
+            // So the convenience is worth it.
+            WeatherContract.WeatherEntry.TABLE_NAME + "." + WeatherContract.WeatherEntry._ID,
+            WeatherContract.WeatherEntry.COLUMN_DATE,
+            WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+    };
+
+    // These indices are tied to FORECAST_COLUMNS.  If FORECAST_COLUMNS changes, these
+    // must change.
+    static final int COL_WEATHER_ID = 0;
+    static final int COL_WEATHER_DATE = 1;
+    static final int COL_WEATHER_DESC = 2;
+    static final int COL_WEATHER_MAX_TEMP = 3;
+    static final int COL_WEATHER_MIN_TEMP = 4;
 
     public WeatherDetailFragment() {
         // Required empty public constructor
@@ -37,10 +66,6 @@ public class WeatherDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_weather_detail, container, false);
 
-        Intent intent = getActivity().getIntent();
-        detailWeatherText = intent.getExtras().getString("detailWeatherText");
-        TextView detailWeatherTextView = (TextView) view.findViewById(R.id.detailWeatherText);
-        detailWeatherTextView.setText(detailWeatherText);
         // Inflate the layout for this fragment
         return view;
     }
@@ -55,10 +80,57 @@ public class WeatherDetailFragment extends Fragment {
         setShareIntent();
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        getLoaderManager().initLoader(FORECAST_LOADER_ID, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Intent intent = getActivity().getIntent();
+        if(intent != null) {
+            return new CursorLoader(
+                    getActivity(),
+                    intent.getData(),
+                    FORECAST_COLUMNS,
+                    null,
+                    null,
+                    null
+            );
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        if (!cursor.moveToFirst()){
+            return;
+        }
+        String dateString = Utility.formatDate(cursor.getLong(COL_WEATHER_DATE));
+        String description = cursor.getString(COL_WEATHER_DESC);
+        boolean isMetric = Utility.isMetric(getActivity());
+        String high = Utility.formatTemperature(cursor.getDouble(COL_WEATHER_MAX_TEMP), isMetric);
+        String low = Utility.formatTemperature(cursor.getDouble(COL_WEATHER_MIN_TEMP), isMetric);
+        mDetailWeatherText =String.format("%s - %s - %s/%s", dateString, description, high, low);
+        TextView detailWeatherTextView = (TextView) getView().findViewById(R.id.detailWeatherText);
+        detailWeatherTextView.setText(mDetailWeatherText);
+
+        if(mShareActionProvider != null) {
+            setShareIntent();
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+
     private void setShareIntent () {
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-        shareIntent.putExtra(Intent.EXTRA_TEXT, detailWeatherText);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mDetailWeatherText);
         shareIntent.setType("text/plain");
         if (mShareActionProvider != null) {
             mShareActionProvider.setShareIntent(shareIntent);
